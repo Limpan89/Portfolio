8cce079c0a004944497566b2b66520ab4e7604bf
Limpan89 <lbroback89@gmail.com>
2024-12-24 12:32:39 +0100

    Created HeaderNavItem component

---
+++ portfolio/src/components/HeaderNavItem.jsx
@@ -0,0 +1,11 @@
+
+function HeaderNavItem({text}) {
+
+  return (
+    <li className="nav-item">
+      <a className="nav-link" href="#">{text}</a>
+    </li>
+  )
+}
+
+export default HeaderNavItem
