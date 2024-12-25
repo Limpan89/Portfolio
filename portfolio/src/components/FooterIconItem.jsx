7b9a19cbe1f83cce079ccaf114a8a4c32a8ca05e
Limpan89 <lbroback89@gmail.com>
2024-12-26 00:17:58 +0100

    Increased Size of footer icons

--- portfolio/src/components/FooterIconItem.jsx
+++ portfolio/src/components/FooterIconItem.jsx
@@ -2,7 +2,7 @@
 function FooterIconItem({link, icon, color}) {
 
   return (
-    <a className={`m-1 text-${color}`} href={link}>
+    <a className={`m-1 fs-3 text-${color}`} href={link}>
       {icon}
     </a>
   )
