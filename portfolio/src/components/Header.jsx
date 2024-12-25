eb813929296322d54e9479c472ce30624334568d
Limpan89 <lbroback89@gmail.com>
2024-12-26 00:02:09 +0100

    Added name to header

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
     <header className="sticky-top">
       <nav className="navbar navbar-expand-lg bg-dark bottom-border border-body" data-bs-theme="dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#">Linus Brobäck</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
