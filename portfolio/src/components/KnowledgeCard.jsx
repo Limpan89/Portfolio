7c7f10de1fc97e9a270846fdcff4079bd0fb521e
Limpan89 <lbroback89@gmail.com>
2024-12-26 21:55:21 +0100

    Bootstrap adjustments

--- portfolio/src/components/KnowledgeCard.jsx
+++ portfolio/src/components/KnowledgeCard.jsx
@@ -3,13 +3,14 @@ import './KnowledgeCard.css'
 function KnowledgeCard({mainIcon, minorIcons, title, text}) {
 
   return (
-    <div className="card col-12 col-md-6 col-lg-4 bg-light knowledge-card">
-      <div className="display-1 text-center">{mainIcon}</div>
+    <div className="card col-12 col-md-6 col-lg-4 bg-light knowledge-card w-25 mx-1">
+      <div className="display-1 text-center pt-3">{mainIcon}</div>
       <div className="card-body">
         <h5 className="card-title text-center">{title}</h5>
         <p className="card-text">{text}</p>
       </div>
-      <div className="container d-flex justify-content-around display-5">
+      <hr />
+      <div className="container d-flex justify-content-around display-5 mb-3">
         {minorIcons}
       </div>
     </div>
