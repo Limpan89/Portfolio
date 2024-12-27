2c6e38c0633f3e7417fb5eb27ef48a610b1294b9
Limpan89 <lbroback89@gmail.com>
2024-12-27 01:38:00 +0100

    added router navigation

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,49 @@
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import Home from './components/Home'
+import AboutMe from './components/AboutMe'
+import Knowledge from './components/Knowledge'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    errorElement: <div>Error!!!</div>,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/aboutme',
+        element: <AboutMe />
+      },
+      {
+        path: '/kunskap',
+        element: <Knowledge />
+      }
+    ]
+  }
+])
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Footer />
+      <RouterProvider router={router}>
+
+      </RouterProvider>
     </>
   )
 }
