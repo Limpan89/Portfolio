fdaa3f8d2cf4ba4b1136576d7578534aecd14ed4
Limpan89 <lbroback89@gmail.com>
2024-12-28 19:19:02 +0100

    added g-3 to row div

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
         <hr />
         <p className="lead">Hej! Välkommen till min profilsida! Jag är en .Net student på Nackademins Yrkeshögskola.</p>
       </div>
-      <div className="row d-flex justify-content-center py-5">
+      <div className="row d-flex justify-content-center py-5 g-3">
         <div className="col-md-8 col-lg-6 col-xl-5">
           <img src="./src/assets/ProfileImage.jpg" alt="Profilbild" />
         </div>
