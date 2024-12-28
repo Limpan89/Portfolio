e033a4d42bcba6364a91c115ae567f9d72f2f635
Limpan89 <lbroback89@gmail.com>
2024-12-28 11:09:59 +0100

    fixed so navbar-brand links to home page

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import HeaderNavItem from "./HeaderNavItem"
 import { Download } from "react-bootstrap-icons"
+import { Link } from "react-router-dom"
 
 function Header() {
 
@@ -7,7 +8,7 @@ function Header() {
     <header className="sticky-top">
       <nav className="navbar navbar-expand-lg bg-dark bottom-border border-body" data-bs-theme="dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Linus Brobäck</a>
+          <Link className="navbar-brand text-warning" to="/">Linus Brobäck</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
