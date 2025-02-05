44880f8a3482707bf1a3dfa1dd447fea30572c98
Limpan89 <lbroback89@gmail.com>
2025-02-05 13:39:51 +0100

    base url

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
 function Home() {
+  const BASE_URL = import.meta.env.BASE_URL;
+  console.log(BASE_URL);
   return (
     <main className="container mt-5">
       <div className="text-center">
@@ -11,6 +13,8 @@ function Home() {
       </div>
       <div className="row d-flex justify-content-center py-5 g-3">
         <div className="col-md-8 col-lg-6 col-xl-5">
+          <img src={BASE_URL + "profileImage.JPG"} alt="Profilbild" />
+          <img src={BASE_URL + "public/profileImage.JPG"} alt="Profilbild" />
           <img src="public/profileImage.JPG" alt="Profilbild" />
         </div>
       </div>
